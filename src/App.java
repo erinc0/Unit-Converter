b5b05e3d3cbc34d8e1ff1ac1eb2fef2648b41486
Erin Cook <don@Ians-iMac.wowway.com>
2023-11-14 22:42:01 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,67 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class App extends Application { 
+  private Label meowLabel;
+  private Label miauLabel;
+  public int x;
+  public int y;
+  public String miau;
+  private TextField meowfield;
+  private TextField miauField;
+  private Button meowButton;
+  @Override
+    public void start (Stage primaryStage){
+
+      Scene meowscene = null;
+      GridPane gridmeow= null;
+      gridmeow = new GridPane();
+      meowscene = new Scene(gridmeow);
+      meowLabel= new Label("km:");
+      miauLabel = new Label("= yards");
+      meowfield = new TextField();
+      meowfield.setEditable(true);
+      meowfield.setText("0");
+      miauField = new TextField();
+      miauField.setEditable(true);
+      miauField.setText("0");
+
+      meowButton = new Button("convert");
+      gridmeow.setPadding(new Insets (5, 5, 5, 5));
+      gridmeow.setHgap(5);
+      gridmeow.add(meowLabel, 0 , 0);
+      gridmeow.add(meowfield, 1 , 0);
+      gridmeow.add(miauField, 1, 1);
+      gridmeow.add(miauLabel, 0, 1);
+      gridmeow.add(meowButton, 0 ,4);
+
+      meowButton.setOnAction(new EventHandler<ActionEvent>() {
+        
+        @Override
+        public void handle(ActionEvent meowEvent){
+           int x;
+           int y;
+          miau = meowfield.getText();
+          x = Integer.parseInt(miau);
+          y = (int) (1093.61 * x);
+          miauField.setText(Integer.toString(y));
+        }
+      }
+      );
+      primaryStage.setScene(meowscene);
+      primaryStage.setTitle("Conversion Calculator");
+      primaryStage.show();
+    }
+    
+    public static void main(String[] args) throws Exception {
+     launch(args);
+}
+}
